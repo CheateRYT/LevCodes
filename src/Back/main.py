47a207917562ed66e9905252c54ab2bde3025b5b
Arkofnights <quigonark@gmail.com>
2024-02-13 22:20:02 +0300

    Created main.py

--- src/Back/main.py
+++ src/Back/main.py
@@ -12,11 +12,13 @@ try:
     print('Sucessfuly connection...')
     print("#" * 20)
     try:
-        with connection.cursor() as cursor:
-            create_table_query = "CREATE TEABLE `users`(id int AUTO_INCREMENT, " \
-                                 "name varchar(32), " \
-                                 "password varchar(32), " \
-                                 "email varchar(32)"; \
+        wwith connection.cursor() as cursor:
+    create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT," \
+                            " name varchar(32)," \
+                            " password varchar(32)," \
+                            " email varchar(32), PRIMARY KEY (id));"
+    cursor.execute(create_table_query)
+    print("Table created successfully")
     finally:
         connection.close()
 except Exception as ex:
