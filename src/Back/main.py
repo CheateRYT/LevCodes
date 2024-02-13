b36353033a160a7decb94f7e7af299dcd956f6f4
Arkofnights <quigonark@gmail.com>
2024-02-13 21:30:17 +0300

    Created main.py

--- src/Back/main.py
+++ src/Back/main.py
@@ -1,1 +1,21 @@
-print('ffafafaf')
+import pymysql
+
+conn = pymysql.connect(
+    host='localhost',
+    user='myuser',
+    password='mypassword',
+    database='mydatabase'
+)
+# Установка соединения с базой данных
+
+cursor = conn.cursor()
+# Создание курсора
+
+cursor.execute("SELECT * FROM mytable")
+# Выполнение SQL-запроса
+
+results = cursor.fetchall()
+# Получение результатов
+
+conn.close()
+# Закрытие соединения
